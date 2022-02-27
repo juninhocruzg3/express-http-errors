8dd2eecd728c7d3b6a54366e278cced8f5215c0e
Juninho Cruz <antoniojr.ufam@gmail.com>
2022-02-26 22:32:45 -0300

    fix: return after send error response

--- src/express-error-handler.ts
+++ src/express-error-handler.ts
@@ -3,8 +3,7 @@ import { HttpError } from './HttpError';
 
 export const ExpressErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     if (err instanceof HttpError) {
-        (err as HttpError).send(res);
+        return (err as HttpError).send(res);
     }
-
     return next(err);
 }
